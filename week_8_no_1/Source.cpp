57e2ee7bb03f5c3d1b865319d46e0c60de7af3bd
pawida sattayamuk <TangTana@hotmail.com>
2020-10-18 19:20:02 +0700

    commit1

--- week_8_no_1/Source.cpp
+++ week_8_no_1/Source.cpp
@@ -1,11 +1,11 @@
 #include<stdio.h>
 
 void draw(int n,int x) {
-		for (int j = 1; j <= x; j++) {
-			if (j != ((x + 1) / 2) - n && j != ((x + 1) / 2) + n) {
+		for (int j = 1; j <= 2*x-1; j++) {
+			if (j != x - n && j != x + n) {
 				printf("  ");
 			}
-			if (j == ((x + 1) / 2) - n || j == ((x + 1) / 2) + n) {
+			if (j == x - n || j == x + n) {
 				printf("* ");
 			}
 		}
@@ -18,13 +18,13 @@ int main(){
 	scanf("%d", &x);
 	int n = 0;
 	
-	for (int i=1; i <= (x + 1) / 2; i++) {
+	for (int i=1; i <= x; i++) {
 		draw(n,x);
 		n++;
 	}
 	
 	int m = n-2;
-	for (int i = 1; i <= ((x + 1) / 2)-1; i++) {
+	for (int i = 1; i <= x-1; i++) {
 		draw(m, x);
 		m--;
 	}
